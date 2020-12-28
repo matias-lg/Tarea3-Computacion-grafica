5e5d713f49015aa2b54c3cbb3f226577509cd5f6
mati <matilop965@gmail.com>
2020-12-28 16:41:16 -0300

    Manipulacion de JSON terminada

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import json
 
 with open('virus.json') as f:
 	data = json.load(f)
+inputs = data[0]
+radius = inputs['Radius']
+prob_con = inputs['Contagious_prob']
+death_rate = inputs['Death_rate']
+poblacion = inputs['Initial_population']
+heal_days = inputs['Days_to_heal']
 
-print(data)
-
-for i in [1,2,3,4]:
-	print(i)
+print(radius, prob_con, death_rate, poblacion, heal_days)
