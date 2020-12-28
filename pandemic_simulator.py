1f2af1e30ed378fcc5a5b335c2d0d3810c401019
mati <matilop965@gmail.com>
2020-12-28 17:02:27 -0300

    Ahora si

--- pandemic_simulator.py
+++ pandemic_simulator.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import json
-
 
 # Variables iniciales
 with open('virus.json') as f:
@@ -13,19 +11,3 @@ poblacion = inputs['Initial_population']
 heal_days = inputs['Days_to_heal']
 
 print(radius, prob_con, death_rate, poblacion, heal_days)
-=======
-import json
-
-
-# Variables iniciales
-with open('virus.json') as f:
-	data = json.load(f)
-inputs = data[0]
-radius = inputs['Radius']
-prob_con = inputs['Contagious_prob']
-death_rate = inputs['Death_rate']
-poblacion = inputs['Initial_population']
-heal_days = inputs['Days_to_heal']
-
-print(radius, prob_con, death_rate, poblacion, heal_days)
->>>>>>> f45a81776bf66a0e3f0cf964c0a7e1c74e08085e
